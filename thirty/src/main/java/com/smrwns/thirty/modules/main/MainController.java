afb495aa45b2b7b4acd89ea2cd467ad634ba93a6
smrwns <smrwns@naver.com>
2015-01-31 01:17:04 +0900

    동작 확인을 위한 정리

--- thirty/src/main/java/com/smrwns/thirty/modules/main/MainController.java
+++ thirty/src/main/java/com/smrwns/thirty/modules/main/MainController.java
@@ -1,12 +1,18 @@
 package com.smrwns.thirty.modules.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	MainService mainService;
+	
 	@RequestMapping("/main")
 	public void main() {
-		System.out.println("main execute");
+		System.out.println("main controller execute");
+		mainService.main();
 	}
 }
---
+++ thirty/src/main/java/com/smrwns/thirty/modules/main/MainService.java
@@ -0,0 +1,12 @@
+package com.smrwns.thirty.modules.main;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+
+	public void main() {
+		System.out.println("main service execute");
+	}
+
+}
