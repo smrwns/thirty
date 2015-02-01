ce75fdebd3275f25d6c39bb7148033b5b49a7d61
smrwns <smrwns@naver.com>
2015-02-01 14:28:43 +0900

    controller

--- thirty/src/main/java/com/smrwns/thirty/modules/board/BoardController.java
+++ thirty/src/main/java/com/smrwns/thirty/modules/board/BoardController.java
@@ -1,10 +1,31 @@
 package com.smrwns.thirty.modules.board;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smrwns.thirty.domains.Board;
+import com.smrwns.thirty.modules.board.support.BoardSearchContext;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping("/list")
+	public String list(BoardSearchContext searchContext, Model model) {
+		List<Board> boardList = boardService.getBoardListByContext(searchContext);
+
+		model.addAttribute("boardList", boardList);
+		model.addAttribute("searchContext", searchContext);
+		
+		return "/board/list";
+	}
+	
+	
 }
