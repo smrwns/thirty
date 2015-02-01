d723e91f3835010b2335a0565d8b5120d7c27742
smrwns <smrwns@naver.com>
2015-02-01 14:53:20 +0900

    정리

--- thirty/src/main/java/com/smrwns/thirty/domains/Board.java
+++ thirty/src/main/java/com/smrwns/thirty/domains/Board.java
@@ -1,5 +1,38 @@
 package com.smrwns.thirty.domains;
 
-public class Board {
+import java.util.Date;
 
+import org.apache.ibatis.type.Alias;
+
+@Alias("Board")
+public class Board {
+	private Long id;
+	private String title;
+	private String desc;
+	private Date create_dt;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public Date getCreate_dt() {
+		return create_dt;
+	}
+	public void setCreate_dt(Date create_dt) {
+		this.create_dt = create_dt;
+	}
 }
