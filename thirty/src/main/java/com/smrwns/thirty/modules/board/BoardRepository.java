ef333357b2f02e6b7b08b44f72c78b15272ba56d
smrwns <smrwns@naver.com>
2015-02-01 14:31:28 +0900

    repository

--- thirty/src/main/java/com/smrwns/thirty/modules/board/BoardRepository.java
+++ thirty/src/main/java/com/smrwns/thirty/modules/board/BoardRepository.java
@@ -1,5 +1,16 @@
 package com.smrwns.thirty.modules.board;
 
-public class BoardRepository {
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.smrwns.thirty.domains.Board;
+import com.smrwns.thirty.modules.board.support.BoardSearchContext;
+
+@Repository
+public interface BoardRepository {
+
+	public long getTotalRowCountBySearchContext(BoardSearchContext searchContext);
+	public List<Board> getListBySearchContext(BoardSearchContext searchContext);
 
 }
