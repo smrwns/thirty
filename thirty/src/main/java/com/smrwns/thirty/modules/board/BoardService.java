2a68d6132aa5a8c31d410659e738b27d79717399
smrwns <smrwns@naver.com>
2015-02-01 14:30:54 +0900

    service

--- thirty/src/main/java/com/smrwns/thirty/modules/board/BoardService.java
+++ thirty/src/main/java/com/smrwns/thirty/modules/board/BoardService.java
@@ -1,5 +1,28 @@
 package com.smrwns.thirty.modules.board;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.smrwns.thirty.domains.Board;
+import com.smrwns.thirty.modules.board.support.BoardSearchContext;
+
+@Service
 public class BoardService {
+
+	@Autowired
+	private BoardRepository boardRepository;
+	
+	@Transactional(readOnly = true)
+	public List<Board> getBoardListByContext(BoardSearchContext searchContext) {
+		
+		long p_totalcount = boardRepository.getTotalRowCountBySearchContext(searchContext);
+		searchContext.setP_totalcount(p_totalcount);
+		List<Board> boardList = boardRepository.getListBySearchContext(searchContext);
+		
+		return boardList;
+	}
 
 }
